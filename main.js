7423a9e464a87e01420576f9f5491cb633c8c27b
ledek89 <ledek89@gmail.com>
2018-06-05 18:21:10 +0200

    hiding menu correctly on mobile version

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
   $(".nav a").on("click", function(){
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
+    $('.navbar-collapse.in').collapse('hide');
+  });
+  $("body").on("click", function(){
+    $('.navbar-collapse.in').collapse('hide');
   });
 });
